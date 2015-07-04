eac4ccbc0dc6c3de9de3308390144e3e5729033b
Jonathan Gray <j.gray@soton.ac.uk>
2015-07-04 17:33:51 +0100

    Revert "Spin idents"
    This reverts commit f975511bea52742b0f6e503a7f542b501a116ba2.

--- disclosuregame/Games/sharing.py
+++ disclosuregame/Games/sharing.py
@@ -74,9 +74,7 @@ class CarryingInformationGame(CarryingReferralGame):
         self.pop_count = len(women)
         self.signaller_generator = self.signaller_fn.generator(random=self.player_random, type_distribution=self.women_weights, 
             agent_args=self.signaller_args, initor=self.signaller_initor,init_args=self.signaller_init_args)
-        
-        while self.signaller_fn.id_generator.next() != self.gen_reset:
-            LOG.debug("Spinning up idents.")
+        self.signaller_fn.id_generator = count(self.gen_reset)
         LOG.debug("Made player generator.")
         self.random.shuffle(women)
         self.num_midwives = len(midwives)
